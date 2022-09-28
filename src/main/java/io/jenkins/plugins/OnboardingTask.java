21672496e3b28aa5e190a3490d9f0acb2d829b5a
Julie Heard <55280278+julieheard@users.noreply.github.com>
2022-09-28 15:31:45 +0100

    rejigged HTTP request methods
    - I removed the connection data so I only store the data temporarily
    - Rejigged the two HTTP methods
    - Included a new variable for storing the payload temporarily whilst sending it
    - Tested and working in Jenkins

--- src/main/java/io/jenkins/plugins/OnboardingTask.java
+++ src/main/java/io/jenkins/plugins/OnboardingTask.java
@@ -6,14 +6,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.springframework.http.HttpHeaders;
 
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.verb.POST;
+import org.springframework.http.HttpHeaders;
 
 import hudson.Extension;
 import hudson.ExtensionList;
@@ -27,62 +28,50 @@ import jenkins.model.GlobalConfiguration;
 @Extension
 public class OnboardingTask extends GlobalConfiguration {
 
-    /** @return the singleton instance */
-    public static OnboardingTask get() {
-        return ExtensionList.lookupSingleton(OnboardingTask.class);
-    }
+    private String temporaryPayload;
 
     private String name;
     private String description;
-    private ConnectionData connectionData;
+
+    /**
+     * @return the singleton instance
+     */
+    public static OnboardingTask get() {
+        return ExtensionList.lookupSingleton(OnboardingTask.class);
+    }
 
     public OnboardingTask() {
         // When Jenkins is restarted, load any saved configuration from disk.
         load();
     }
 
+    /**
+     * This method does exactly the same as doTestConnection() but also sends a payload.
+     * There is a global temporaryPayload String which is set here and then erased when the payload is sent
+     * in sendHTTPRequest()
+     */
     @POST
     public FormValidation doTestConnectionWithPayload(@QueryParameter("username") String username,
                                                       @QueryParameter("password") Secret password,
-                                                      @QueryParameter("url") String url) throws IOException {
-        try {
-            String errorMessage = validateQueryParams(url, username);
-            if (errorMessage.length() != 0) {
-                return FormValidation.warning(errorMessage);
-            } else {
-
-                setConnectionData(username, password, url);
-                connectionData.setPayload("Secret Text");
-
-                //This runs an HTTP request on this objects URL, username and password and returns an HTTP response code
-                int httpRequestResponseCode = sendHTTPRequest();
-
-                if (httpRequestResponseCode != 200) {
-                    return FormValidation.warning(
-                            "Connection refused, Http warning code: " + httpRequestResponseCode + "."
-                            + " Please check your URL and credentials and try again");
-                }
-            }
-
-            return FormValidation.ok("Success!");
-        } catch (Exception e) {
-            return FormValidation.error("Client error : " + e.getMessage());
-        }
+                                                      @QueryParameter("url") String url) {
+        temporaryPayload = password.getPlainText();
+        return doTestConnection(username, password, url);
     }
 
     @POST
     public FormValidation doTestConnection(@QueryParameter("username") String username,
                                            @QueryParameter("password") Secret password,
-                                           @QueryParameter("url") String url) throws IOException {
+                                           @QueryParameter("url") String url) {
         try {
+            //I have grouped the validation calls and error message results into validateQueryParams
             String errorMessage = validateQueryParams(url, username);
+            //If errors, return warning, otherwise safe to send HTTP request and return success
             if (errorMessage.length() != 0) {
                 return FormValidation.warning(errorMessage);
             } else {
-                setConnectionData(username, password, url);
 
                 //This runs an HTTP request on this objects URL, username and password and returns an HTTP response code
-                int httpRequestResponseCode = sendHTTPRequest();
+                int httpRequestResponseCode = sendHTTPRequest(new URL(url), username, password);
 
                 if (httpRequestResponseCode != 200) {
                     return FormValidation.warning(
@@ -90,33 +79,36 @@ public class OnboardingTask extends GlobalConfiguration {
                             + " Please check your URL and credentials and try again");
                 }
             }
-
             return FormValidation.ok("Success!");
         } catch (Exception e) {
             return FormValidation.error("Client error : " + e.getMessage());
         }
     }
-
 
     /**
      * This runs an HTTP request on this objects URL, username and password and returns an HTTP response code
      *
      * @return the int returned represents the response code from the HTTP request
      */
-    private int sendHTTPRequest() throws IOException {
+    private int sendHTTPRequest(URL url, String username, Secret password) throws IOException {
         int responseCode = 0;
-        HttpURLConnection httpURLConnection = (HttpURLConnection) connectionData.getUrl().openConnection();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         httpURLConnection.setRequestMethod("POST");
-        httpURLConnection.setRequestProperty(HttpHeaders.AUTHORIZATION, connectionData.getFormattedCredentials());
+        httpURLConnection.setRequestProperty(HttpHeaders.AUTHORIZATION, getFormattedCredentials(username, password));
 
         //If the payload has data in it, add the payload to the HTTP connection
-        if (connectionData.getPayload().length() != 0) {
-            //This adds a payload to the HTTP request
-            httpURLConnection.setDoOutput(true);
-            OutputStream os = httpURLConnection.getOutputStream();
-            os.write(connectionData.getPayload().getBytes());
-            os.flush();
-            os.close();
+        try {
+            if (temporaryPayload.length() != 0) {
+                //This adds a payload to the HTTP request
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                os.write(temporaryPayload.getBytes());
+                os.flush();
+                os.close();
+                temporaryPayload = ""; //Set payload back to empty
+            }
+        } catch (NullPointerException e) {
+            //This means the String is empty so no payload needs to be added
         }
 
         responseCode = httpURLConnection.getResponseCode();
@@ -124,17 +116,14 @@ public class OnboardingTask extends GlobalConfiguration {
     }
 
     /**
-     * This sets the global connectionData object based on the query params
-     * @param username
-     * @param password
-     * @param url This URL has been checked for format in validateQueryParams() using checkForValidURL()
-     *            before it gets here so should never throw a malformedURL exception
-     * @throws MalformedURLException
+     * Formats the username and password for basic authentication
+     *
+     * @return "Basic username : password" where "username : passsword" is a base 64 encoded string
      */
-    private void setConnectionData(String username, Secret password, String url) throws MalformedURLException {
-        URL newURL = new URL(url);
-        connectionData = new ConnectionData(username, password, newURL);
-        save();
+    public String getFormattedCredentials(String username, Secret password) {
+        String credentials = Base64.getEncoder().encodeToString(
+                (username + ":" + password).getBytes(Charset.forName("UTF-8")));
+        return "Basic " + credentials;
     }
 
     private String validateQueryParams(String url, String username) {
@@ -213,16 +202,5 @@ public class OnboardingTask extends GlobalConfiguration {
         return description;
     }
 
-    public ConnectionData getConnectionData() {
-        return this.connectionData;
-    }
-
-    @DataBoundSetter
-    public void setConnectionData(ConnectionData connectionData) {
-        if (connectionData.getUsername().length() != 0 && nameFormatCheck(connectionData.getUsername())) {
-            this.connectionData = connectionData;
-            save();
-        }
-    }
 
 }
